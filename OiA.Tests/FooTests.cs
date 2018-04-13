739d915a52353b41819cee34aff291861d6ce58d
Ranu <ranu.miah@outlook.com>
2018-04-14 00:21:22 +0100

    Add test project

---
+++ OiA.Tests/FooTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace OiA.Tests
+{
+    [TestFixture]
+    public class FooTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
