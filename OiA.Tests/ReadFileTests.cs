2cfd1d84f6dbf092a258be1c630ff58514402f4e
Ranu <ranu.miah@outlook.com>
2018-04-16 21:55:46 +0100

    Add hasing and reading file details

---
+++ OiA.Tests/ReadFileTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace OiA.Tests
+{
+    [TestFixture]
+    public class ReadFileTests
+    {
+        string testDirectory = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\..\TestData"));
+
+        [Test]
+        public void ReadFileDataTest()
+        {
+            ReadFile readFile = new ReadFile();
+
+            FileDetail fileDetails = readFile.GetFileDetails(Path.Combine(testDirectory, "Aquatic Flowers.jpg"));
+
+            Assert.That(fileDetails.FileName, Is.EqualTo("Aquatic Flowers.jpg"));
+            Assert.That(fileDetails.FileExtension, Is.EqualTo(".jpg"));
+            Assert.That(fileDetails.FileLength, Is.EqualTo(453438));
+            Assert.That(fileDetails.FileFullName, Is.EqualTo(@"C:\code\OiA\TestData\Aquatic Flowers.jpg"));
+            Assert.That(fileDetails.FileLastWriteTimeUtc, Is.EqualTo(new DateTime(2015,09,21,11,28,40)));
+        }
+
+        [Test]
+        public void CreateFileMd5Hash()
+        {
+            var md5Hash = HashFile.GenerateMd5Hash(Path.Combine(testDirectory, "Aquatic Flowers.jpg"));
+
+            Assert.That(md5Hash, Is.EqualTo("E? <??R\\!N%?=ZO?"));
+        }
+
+        [Test]
+        public void CreateFileSha256Hash()
+        {
+            var sha256Hash = HashFile.GenerateSHA256String(Path.Combine(testDirectory, "Aquatic Flowers.jpg"));
+
+            Assert.That(sha256Hash, Is.EqualTo("0EA9E367888A6A8C6BEF55C5B10223346C9F8126F631CB39B61C974517D57CE8"));
+        }
+
+        [Test]
+        public void CreateFileSha512Hash()
+        {
+            var sha256Hash = HashFile.GenerateSHA512String(Path.Combine(testDirectory, "Aquatic Flowers.jpg"));
+
+            Assert.That(sha256Hash, Is.EqualTo("08696184B3DF7AF07EA1373831E83EA4F85AA5619FB0A99FFA50BA56A46B79862192426ABE9F4E600FCEC6649A8A5F0B1EB2DF5B1202CECF4A309C77E2BD9F12"));
+        }
+    }
+}
---
+++ OiA/ReadFile.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OiA
+{
+    public static class HashFile
+    {
+        public static string GenerateMd5Hash(string input)
+        {
+            using (var hash = MD5.Create())
+            {
+                var result = hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return Encoding.ASCII.GetString(result);
+            }
+        }
+
+        public static string GenerateSHA256String(string inputString)
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(inputString);
+            byte[] hash = sha256.ComputeHash(bytes);
+            return GetStringFromHash(hash);
+        }
+
+        public static string GenerateSHA512String(string inputString)
+        {
+            SHA512 sha512 = SHA512.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(inputString);
+            byte[] hash = sha512.ComputeHash(bytes);
+            return GetStringFromHash(hash);
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+            return result.ToString();
+        }
+
+    }
+
+    public class ReadFile
+    {
+        public FileDetail GetFileDetails(string file)
+        {
+            return new FileDetail(file);
+        }
+    }
+
+    public class FileDetail
+    {
+
+        public FileDetail(string filePath)
+        {
+            var file = new FileInfo(filePath);
+            FileName = file.Name;
+            FileLength = file.Length;
+            FileFullName = file.FullName;
+            FileExtension = file.Extension;
+            FileCreationTimeUtc = file.CreationTimeUtc;
+            FileLastWriteTimeUtc = file.LastWriteTimeUtc;
+            FileLastAccessTimeUtc = file.LastAccessTimeUtc;
+
+
+        }
+
+        public string FileName { get; }
+        public long FileLength { get; }
+        public string FileFullName { get; }
+        public string FileExtension { get; }
+        public DateTime FileCreationTimeUtc { get; }
+        public DateTime FileLastWriteTimeUtc { get; }
+        public DateTime FileLastAccessTimeUtc { get; }
+    }
+}
