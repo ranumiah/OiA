a334bb06eb214a7bfba13b4a11ebae6c43944ce7
Ranu <O@O.net>
2018-04-18 11:23:57 +0100

    Add Nlog

--- OiA/Program.cs
+++ OiA/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using NLog;
 
 namespace OiA
 {
     class Program
     {
+        static readonly Logger Logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Logger.Debug("init main");
+                Logger.Debug("Hello World!");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
     }
 }
